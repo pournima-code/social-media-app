9e3fbf981f57019696e4142bd4a6904b35d32c14
Pournima Padelkar <padelkarpournima20@gmail.com>
2023-02-18 17:33:30 +0530

    Topbar implementation

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import Topbar from '../Components/Topbar/Topbar'
+import './Home.css'
+
+export default function Home() {
+  return (
+    <>
+    <Topbar/>
+      <div className="homeContainer">
+        hi
+      </div>
+    </>
+  )
+}
